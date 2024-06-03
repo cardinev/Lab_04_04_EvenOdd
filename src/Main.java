1710f2411209383a998234570b4a9cb78ac67dc8
Caroline Velten <veltencf@mail.uc.edu>
2024-06-02 20:53:24 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int numberToExamine = 5;
+    int modulo = numberToExamine % 2;
+    if (modulo == 0){
+           System.out.println( "Your number is even");
+    }
+    else {System.out.println( " Your number is odd" );}
+    }
+}
